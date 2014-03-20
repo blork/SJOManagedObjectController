87ac138265dec04fb073d871821ff8f5e890b100
Sam Oakley <sam@blork.co.uk>
2014-03-20 19:50:34 +0000

    Initial Commit

---
+++ SJOManagedObjectController/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SJOManagedObjectController
+//
+//  Created by Sam Oakley on 20/03/2014.
+//  Copyright (c) 2014 Sam Oakley. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ SJOManagedObjectControllerTests/SJOManagedObjectControllerTests.m
@@ -0,0 +1,34 @@
+//
+//  SJOManagedObjectControllerTests.m
+//  SJOManagedObjectControllerTests
+//
+//  Created by Sam Oakley on 20/03/2014.
+//  Copyright (c) 2014 Sam Oakley. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SJOManagedObjectControllerTests : XCTestCase
+
+@end
+
+@implementation SJOManagedObjectControllerTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
